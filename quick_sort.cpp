14e2c928dceadd3ab2ec947108525cf5655b8161
Anshul Gupta <anshulmgupta@gmail.com>
2017-09-11 00:09:18 -0700

    Quick sort more test cases

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -7,11 +7,11 @@ int partition(int arr[], int start, int end){
     int left = start + 1;
     int right = end;
     while(left <= right){
-        while(arr[left] < arr[p_idx])
+        while(arr[left] <= arr[p_idx])
             left++;
         while(arr[p_idx] < arr[right])
             right--;
-        if(left < right)
+        if(left <= right)
             swap(arr[left], arr[right]);
     }
     swap(arr[p_idx], arr[right]);
@@ -96,6 +96,26 @@ void test_case_one_four(){
     cout << endl;
 }
 
+void test_case_one_five(){
+    int arr[] = {3,5,6,3,5,7};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    quick_sort(arr, 0, size-1);
+    for(int i = 0; i< size; i++){
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
+
+void test_case_one_six(){
+    int arr[] = {3,3,3,3,3,3};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    quick_sort(arr, 0, size-1);
+    for(int i = 0; i< size; i++){
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
+
 void test_case_two(){
     int arr[] = {5,3,9,1,2};
     int size = sizeof(arr)/sizeof(arr[0]);
@@ -146,18 +166,42 @@ void test_case_two_four(){
     cout << endl;
 }
 
+void test_case_two_five(){
+    int arr[] = {3,5,6,3,5,7};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    quick_sort_1(arr, 0, size-1);
+    for(int i = 0; i< size; i++){
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
+
+void test_case_two_six(){
+    int arr[] = {3,3,3,3,3,3};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    quick_sort_1(arr, 0, size-1);
+    for(int i = 0; i< size; i++){
+        cout << arr[i] << ",";
+    }
+    cout << endl;
+}
+
 int main(){
     test_case_one();
     test_case_one_one();
     test_case_one_two();
     test_case_one_three();
     test_case_one_four();
-
+    test_case_one_five();
+    test_case_one_six();
+    cout << "==================================" << endl;
     test_case_two();
     test_case_two_one();
     test_case_two_two();
     test_case_two_three();
     test_case_two_four();
+    test_case_two_five();
+    test_case_two_six();
 
     return 0;
 }
